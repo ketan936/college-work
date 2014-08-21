07d579907da68b8a81e15152ea231a56696fbf1b
Ketan Gupta <ketan936@gmail.com>
2014-08-21 23:59:20 +0530

    start writing code for some_cipher

--- internet-security/some_cipher.cpp
+++ internet-security/some_cipher.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main(){
-string s = "hello";
-cout << s;
+
+string cipher;
+int n;
+
+cout << "Enter a string: ";
+cin >> cipher;
+
+cout << "enter a level of cipher";
+cin >> n;
+
+string parts[n];
+cout << cipher <<endl;
+
+
 }
