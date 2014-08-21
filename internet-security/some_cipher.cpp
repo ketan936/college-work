6dc410cefc2ee588802404d8ec8dd51c859694e1
Ketan Gupta <ketan936@gmail.com>
2014-08-22 01:48:09 +0530

    start writing half code for some_cipher

--- internet-security/some_cipher.cpp
+++ internet-security/some_cipher.cpp
@@ -8,12 +8,41 @@ int n;
 
 cout << "Enter a string: ";
 cin >> cipher;
+cout << cipher <<endl;
 
-cout << "enter a level of cipher";
+cout << "Enter a level of cipher: ";
 cin >> n;
 
 string parts[n];
-cout << cipher <<endl;
+
+for (int i=0;i < cipher.length();i++){
+	int pos=i%n;
+	cout << "pos: " << pos <<endl;
+	parts[pos] += cipher[i];
+	
+} 
+cout << "parts are: " << endl;
+cout << parts[0] <<endl;
+cout << parts[1] <<endl;
+cout << parts[2] <<endl;
+string encrypted;
+for (string s : parts){
+	encrypted += s;
+}
+cout << encrypted <<endl;
+for (int i=3;i==3;i++){
+cout << "Checking for level: " << i << endl;
+string other_parts[i];
+for (int j=0;j<encrypted.length();j++){
+	other_parts[j%i]+=encrypted[j];
+}
+
+cout << "other_parts are: " << endl;
+cout << other_parts[0] <<endl;
+cout << other_parts[1] <<endl;
+cout << other_parts[2] <<endl;
+
+}
 
 
 }
