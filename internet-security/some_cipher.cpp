154189e6b6f9af3176e781cc8a3cea4c00c45809
Ketan Gupta <ketan936@gmail.com>
2014-08-22 13:42:34 +0530

    almost complete

--- internet-security/some_cipher.cpp
+++ internet-security/some_cipher.cpp
@@ -1,48 +1,88 @@
-#include<iostream>
+#include <iostream>
+#include <stack>
 using namespace std;
 
-int main(){
-
-string cipher;
-int n;
-
-cout << "Enter a string: ";
-cin >> cipher;
-cout << cipher <<endl;
-
-cout << "Enter a level of cipher: ";
-cin >> n;
-
-string parts[n];
-
-for (int i=0;i < cipher.length();i++){
-	int pos=i%n;
-	cout << "pos: " << pos <<endl;
-	parts[pos] += cipher[i];
-	
-} 
-cout << "parts are: " << endl;
-cout << parts[0] <<endl;
-cout << parts[1] <<endl;
-cout << parts[2] <<endl;
-string encrypted;
-for (string s : parts){
-	encrypted += s;
-}
-cout << encrypted <<endl;
-for (int i=3;i==3;i++){
-cout << "Checking for level: " << i << endl;
-string other_parts[i];
-for (int j=0;j<encrypted.length();j++){
-	other_parts[j%i]+=encrypted[j];
+stack<int> create_stack(int level) {
+	stack<int> my_stack;
+	for (int i = 1; i < level; ++i) {
+		my_stack.push(i);
+	}
+	for (int i = level - 2; i >= 0; --i) {
+		my_stack.push(i);
+	}
+	return my_stack;
 }
 
-cout << "other_parts are: " << endl;
-cout << other_parts[0] <<endl;
-cout << other_parts[1] <<endl;
-cout << other_parts[2] <<endl;
+string remove_x(string str, char x) {
+	string output;
+	for (size_t i = 0; i < str.size(); ++i)
+		if (str[i] != x)
+			output += str[i];
+	return output;
+}
+
+int main() {
+
+	string cipher;
+	int n;
+
+	cout << "Enter a string: ";
+	cin >> cipher;
+	cout << cipher << endl;
+
+	cout << "Enter a level of cipher: ";
+	cin >> n;
+
+	string parts[n];
+	stack<int> my_stack;
+	for (int i = 0; i < cipher.length(); i++) {
+		if (my_stack.empty())
+			my_stack = create_stack(n);
+		cout << "my stack: " << my_stack.top() << endl;
+		for (int j = 0; j < n; j++)
+			parts[j] += ' ';
+		parts[my_stack.top()][parts[0].length() - 1] = cipher[i];
+		my_stack.pop();
+	}
+	while (!my_stack.empty()) {
+		for (int j = 0; j < n; j++)
+			parts[j] += ' ';
+		parts[my_stack.top()][parts[0].length() - 1] = 'x';
+		my_stack.pop();
+	}
+	for (int j = 0; j < n; j++)
+		parts[j] += ' ';
+	parts[0][parts[0].length() - 1] = 'x';
+
+	cout << "parts are: " << endl;
+	for (int i = 0; i < n; ++i) {
+		cout << parts[i] << endl;
+	}
+
+	string encrypted;
+	for (string s : parts) {
+		encrypted += s;
+	}
+	encrypted = remove_x(encrypted, ' ');
+	cout << "encrypted string is: " << encrypted << endl;
+	for (int i = 3; i == 3; i++) {
+		cout << "Checking for level: " << i << endl;
+		string other_parts[i];
+		int length_of_top= encrypted.length()/i;
+		other_parts[i-1]=encrypted.substr(encrypted.length()-length_of_top-1,length_of_top-1);
+		other_parts[0]=encrypted.substr(0,length_of_top);
+
+		int lenght_of_rest = (length_of_top*2)-2;
+		for (int it = 1; it < i-1; ++it) {
+			other_parts[it]=encrypted.substr(length_of_top+(lenght_of_rest*(it-1)),lenght_of_rest);
+		}
+
+		cout << "other_parts are: " << endl;
+		for (int k = 0; k < i; ++k) {
+				cout << other_parts[k] << endl;
+			}
+
+	}
 
 }
 
-
-}
