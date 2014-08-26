581be4ad4006fdff74352b71d2072c647f87d72f
Ketan Gupta <ketan936@gmail.com>
2014-08-26 11:01:04 +0530

    console.lo

--- ita/validation.js
+++ ita/validation.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-	console.log("outside");
 	$("form").submit(function(event) {
-		console.log("hello");
 		event.preventDefault();
 		alert("Handler for .submit() called.");
 
