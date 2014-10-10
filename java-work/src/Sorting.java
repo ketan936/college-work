ca4b7aad73ef66f3c4dc7be4c97089543a1a0fa3
Ketan Gupta <ketan936@gmail.com>
2014-10-10 12:05:26 +0530

    add some files

---
+++ java-work/src/Sorting.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author castle
+ *
+ */
+public class Sorting {
+
+	
+	public static void main(String[] args) {
+		int [] array = {1,2,3};
+		insertionSort(array);
+	}
+	
+	public static void  insertionSort (int [] list){
+		System.out.println(list.length);
+		
+	}
+
+}
